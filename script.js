bc13ad041baf13561c4d790344502dc762060d98
Annelie <annelie.dorsell@gmail.com>
2022-05-16 14:40:01 +0200

    adding html and js

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// to handle one of the potential errors you can use this built in method:
+const variableName = 10;
+isNaN(variableName);
+
+function atm() {
+  const message = parseFloat(prompt("Display message"));
+  // you need to answer the question why we are using parseFloat() method here
+  // either use a if/else statement or a switch. Write a comment and motivate your choice
+}
